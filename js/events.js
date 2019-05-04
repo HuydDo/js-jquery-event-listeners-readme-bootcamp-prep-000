0e94e00226f820780b3f1f2aed17e1e48573a7e0
Huy do <huyddo40@gmail.com>
2019-05-04 20:26:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if ($( 'input').val() === 'G'){
+      alert('G key was pressed down');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.')
+  });
+}
